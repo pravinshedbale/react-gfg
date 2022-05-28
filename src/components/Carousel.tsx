20f7d90ae2c60c23a155bd228e6c28624e571e5d
Pravin <pravin.shedbale@outlook.com>
2022-05-28 09:05:09 +0530

    carousel implementation completed

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,13 +1,29 @@
 import Card from "./Container/Card";
 import "./Carousel.css";
 import Navigator from "./Navigator";
+import { useState } from "react";
 const Carousel = () => {
+  const carouselImages = [
+    "https://cdn.pixabay.com/photo/2018/05/14/20/56/pansy-3401542_1280.jpg",
+    "https://cdn.pixabay.com/photo/2017/04/07/16/47/spring-2211353_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/09/28/12/35/scotland-1700413_1280.jpg",
+  ];
+  const [carouselIndex, setCaraouselIndex] = useState(0);
   return (
     <>
       <Navigator></Navigator>
       <Card>
-        <div className="carousel"></div>
-        <div className="carousel-controls"></div>
+        <div className="carousel">
+          <img src={carouselImages[carouselIndex]} alt="" height="300" width="400" />
+        </div>
+        <div className="carousel-controls">
+          <button disabled={carouselIndex === 0 ? true : false} onClick={() => setCaraouselIndex(carouselIndex - 1)}>
+            Previous
+          </button>
+          <button disabled={carouselIndex === carouselImages.length - 1 ? true : false} onClick={() => setCaraouselIndex(carouselIndex + 1)}>
+            Next
+          </button>
+        </div>
       </Card>
     </>
   );
