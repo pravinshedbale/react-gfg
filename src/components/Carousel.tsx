d7ae1144595f4adf3c00846ec9a051fd59914416
Pravin <pravin.shedbale@outlook.com>
2022-05-29 15:03:17 +0530

    added images in carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,6 +7,9 @@ const Carousel = () => {
     'https://cdn.pixabay.com/photo/2018/05/14/20/56/pansy-3401542_1280.jpg',
     'https://cdn.pixabay.com/photo/2017/04/07/16/47/spring-2211353_1280.jpg',
     'https://cdn.pixabay.com/photo/2016/09/28/12/35/scotland-1700413_1280.jpg',
+    'https://cdn.pixabay.com/photo/2022/05/16/13/49/cow-7200409_1280.jpg',
+    'https://cdn.pixabay.com/photo/2020/09/16/04/02/skyline-5575251_1280.jpg',
+    'https://cdn.pixabay.com/photo/2015/04/23/22/01/mountains-736886_1280.jpg',
   ];
   const [carouselIndex, setCaraouselIndex] = useState(0);
   return (
